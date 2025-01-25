cd61326cc45bbd646b8bb281cc35b16383db47cc
AkankshaSingal8 <akanksha21008@iiitd.ac.in>
2025-01-25 23:17:39 +0530

    Added height 3

--- change_folder.py
+++ change_folder.py
@@ -17,5 +17,5 @@ def rename_folder(folder_path, num):
         print(f"An error occurred: {e}")
 
 # Usage Example
-folder_path = "height_5_for_mix"
-rename_folder(folder_path, 126)
+folder_path = "height_3"
+rename_folder(folder_path, 146)
